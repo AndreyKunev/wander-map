e67e65cd7dd3f6c9637939bf02a8edc576f376b4
Andrey Kunev <akunev45@gmail.com>
2024-01-22 15:24:52 +0200

    Add: added card component

---
+++ src/components/UsersList/Card/Card.tsx
@@ -0,0 +1,17 @@
+import React, { CSSProperties, ReactNode } from 'react';
+
+import './Card.css';
+
+const Card: React.FC<{
+  className: string;
+  style?: CSSProperties;
+  children?: ReactNode;
+}> = ({ className, style, children }) => {
+  return (
+    <div className={`card ${className}`} style={style}>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
