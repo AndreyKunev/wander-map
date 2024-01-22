fed2a9c7934d6769432f2956e76029c50d17d97c
Andrey Kunev <akunev45@gmail.com>
2024-01-22 14:20:51 +0200

    Add: added avatar component

---
+++ src/components/UsersList/Avatar/Avatar.tsx
@@ -0,0 +1,17 @@
+import "./Avatar.css";
+
+import { AvatarProps } from "../../../types/types";
+
+const Avatar = ({ avatar }: {avatar: AvatarProps}) => {
+  return (
+    <div className={'avatar' + (avatar.className || '')} style={avatar.style}>
+      <img
+        src={avatar.image}
+        alt={avatar.alt}
+        style={{ width: avatar.width, height: avatar.width }}
+      />
+    </div>
+  );
+};
+
+export default Avatar;
