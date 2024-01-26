33ed138ab93a18a2cc8274422b1247ff66356b40
Andrey Kunev <akunev45@gmail.com>
2024-01-26 17:01:14 +0200

    Add: added PlaceList component

---
+++ src/components/PlaceList/PlaceList.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+
+import Card from '../Card/Card';
+import PlaceItem from './PlaceItem/PlaceItem';
+
+import { UserPlaceArray } from '../../types/types';
+
+import './PlaceList.css';
+
+const PlaceList: FC<{ placeArr: UserPlaceArray }> = ({ placeArr }) => {
+  console.log(placeArr);
+
+  if (placeArr.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No Places found.</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {placeArr.map((place) => (
+        <PlaceItem
+          key={place.id}
+          place={place}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
