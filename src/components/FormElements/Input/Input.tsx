03f970bd1ec5fec242b7012eee01680019a805e7
Andrey Kunev <akunev45@gmail.com>
2024-02-06 14:09:16 +0200

    Add: added some validation

--- src/components/FormElements/Input/Input.tsx
+++ src/components/FormElements/Input/Input.tsx
@@ -1,21 +1,82 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, useReducer } from 'react';
+
+import { validate } from '../../../utils/validators';
 
 import './Input.css';
 
-import { InputProps } from '../../../types/types';
+import { InputAction, InputProps, InputState } from '../../../types/types';
+
+const inputReducer = (state: InputState, action: InputAction) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.value,
+        isValid: validate(action.value, action.validators),
+      };
+    case 'TOUCH':
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+};
 
 const Input: FC<InputProps> = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: false,
+    isTouched: false,
+  });
+
+  const changeHandler = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    dispatch({
+      type: 'CHANGE',
+      value: event.target.value,
+      validators: props.validators,
+    });    
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: 'TOUCH',
+      value: '',
+      validators: []
+    });
+  };
+
   const element =
     props.element === 'input' ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3}/>
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     );
 
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && 'form-control--invalid'
+      }`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
     </div>
   );
 };
--- src/pages/NewPlace/NewPlace.tsx
+++ src/pages/NewPlace/NewPlace.tsx
@@ -1,11 +1,18 @@
 import Input from '../../components/FormElements/Input/Input';
+import { VALIDATOR_REQUIRE } from '../../utils/validators';
 
 import './NewPlace.css';
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type='text' label='Title' element='input' />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
     </form>
   );
 };
