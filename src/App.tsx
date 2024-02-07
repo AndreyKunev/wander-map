7381b6b18ea9f642c1562a156a58da098c5f5c60
Andrey Kunev <akunev45@gmail.com>
2024-02-07 23:19:03 +0200

    Add: added routing for update place

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,15 @@ import Wrapper from './pages/Wrapper/Wrapper';
 import Users from './pages/Users/Users';
 import UserPlaces from './pages/UserPlaces/UserPlaces';
 import NewPlace from './pages/NewPlace/NewPlace';
+import UpdatePlace from './pages/UpdatePlace/UpdatePlace';
 
 const appRoutes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Wrapper />}>
       <Route path="/" element={<Users />} />
       <Route path="/:userId/places" element={<UserPlaces />} />
-      <Route path='/places/new' element={<NewPlace />} />
+      <Route path="/places/new" element={<NewPlace />} />
+      <Route path="/places/:placeId" element={<UpdatePlace />} />
     </Route>
   )
 );
