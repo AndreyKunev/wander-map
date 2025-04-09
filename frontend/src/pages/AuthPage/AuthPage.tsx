bccc370b80fadd0efdde6af90f16d9b0ce865313
Andrey Kunev <akunev45@gmail.com>
2025-04-09 21:28:15 +0300

    add#21: added request from frontend

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -4,9 +4,9 @@ import Card from '../../components/Card/Card';
 import Input from '../../components/FormElements/Input/Input';
 import Button from '../../components/FormElements/Button/Button';
 import {
-  VALIDATOR_EMAIL,
-  VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE,
+	VALIDATOR_EMAIL,
+	VALIDATOR_MINLENGTH,
+	VALIDATOR_REQUIRE,
 } from '../../utils/validators';
 import { useForm } from '../../hooks/form-hook';
 import { AuthContext } from '../../context/auth-context';
@@ -14,101 +14,136 @@ import { AuthContext } from '../../context/auth-context';
 import './AuthPage.css';
 
 const AuthPage: FC = () => {
-  const auth = useContext(AuthContext);
-  const [isLogin, setIsLogin] = useState(true);
+	const auth = useContext(AuthContext);
+	const [isLogin, setIsLogin] = useState(true);
 
-  const [formState, inputHandler, setFormData] = useForm(
-    {
-      email: {
-        value: '',
-        isValid: false,
-      },
-      password: {
-        value: '',
-        isValid: false,
-      },
-    },
-    false
-  );
+	const [formState, inputHandler, setFormData] = useForm(
+		{
+			email: {
+				value: '',
+				isValid: false,
+			},
+			password: {
+				value: '',
+				isValid: false,
+			},
+		},
+		false
+	);
 
-  const switchModeHandler = () => {
-    if (!isLogin) {
-      if ('email' in formState.inputs && 'password' in formState.inputs) {
-        setFormData(
-          {
-            ...formState.inputs,
-            name: undefined,
-          },
-          formState.inputs.email.isValid && formState.inputs.password.isValid
-        );
-      }
-    } else {
+	const switchModeHandler = () => {
+		if (!isLogin) {
+			if ('email' in formState.inputs && 'password' in formState.inputs) {
+				setFormData(
+					{
+						...formState.inputs,
+						name: undefined,
+					},
+					formState.inputs.email.isValid &&
+						formState.inputs.password.isValid
+				);
+			}
+		} else {
+			setFormData(
+				{
+					...formState.inputs,
+					name: {
+						value: '',
+						isValid: false,
+					},
+				},
+				false
+			);
+		}
 
-      setFormData(
-        {
-          ...formState.inputs,
-          name: {
-            value: '',
-            isValid: false,
-          },
-        },
-        false
-      );
-    }
+		setIsLogin((prevMode) => !prevMode);
+	};
 
-    setIsLogin((prevMode) => !prevMode);
-  };
+	const loginHandler = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 
-  const loginHandler = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    auth.login();
-  };
+		if (isLogin) {
+      console.log('test');
+      
+		} else {
+      let res;
+			try {
+				if (
+					'name' in formState.inputs &&
+					'email' in formState.inputs &&
+					'password' in formState.inputs
+				) {
+					res = await fetch(
+						'http://localhost:3001/api/users/signup',
+						{
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+							},
+							body: JSON.stringify({
+								name: formState.inputs.name!.value,
+								email: formState.inputs.email.value,
+								password: formState.inputs.password.value,
+							}),
+						}
+					);
+				}
 
-  return (
-    <Card className="authentication">
-      <form className="authentication" onSubmit={loginHandler}>
-        <div className='authentication__header'>
-          <h2>{isLogin ? 'Welcome back!' : 'Create Account'}</h2>
-        </div>
-        {!isLogin && (
-          <Input
-            id="name"
-            type="text"
-            label="Name"
-            element="input"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid name."
-            onInput={inputHandler}
-          />
-        )}
-        <Input
-          id="email"
-          type="text"
-          label="Email"
-          element="input"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email."
-          onInput={inputHandler}
-        />
-        <Input
-          id="password"
-          type="password"
-          label="Password"
-          element="input"
-          validators={[VALIDATOR_MINLENGTH(8)]}
-          errorText="Password is invalid."
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLogin ? 'Login' : 'Register'}
-        </Button>
-      </form>
-      <p>Don't have an account?</p>
-      <Button inverse onClick={switchModeHandler}>
-        {isLogin ? 'Create Account' : 'Switch to Login'}
-      </Button>
-    </Card>
-  );
+				const data = await res!.json();
+				console.log(data);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+
+		auth.login();
+	};
+
+	return (
+		<Card className='authentication'>
+			<form className='authentication' onSubmit={loginHandler}>
+				<div className='authentication__header'>
+					<h2>{isLogin ? 'Welcome back!' : 'Create Account'}</h2>
+				</div>
+				{!isLogin && (
+					<Input
+						id='name'
+						type='text'
+						label='Name'
+						element='input'
+						validators={[VALIDATOR_REQUIRE()]}
+						errorText='Please enter a valid name.'
+						onInput={inputHandler}
+					/>
+				)}
+				<Input
+					id='email'
+					type='text'
+					label='Email'
+					element='input'
+					validators={[VALIDATOR_EMAIL()]}
+					errorText='Please enter a valid email.'
+					onInput={inputHandler}
+				/>
+				<Input
+					id='password'
+					type='password'
+					label='Password'
+					element='input'
+					validators={[VALIDATOR_MINLENGTH(8)]}
+					errorText='Password is invalid.'
+					onInput={inputHandler}
+				/>
+				<Button type='submit' disabled={!formState.isValid}>
+					{isLogin ? 'Login' : 'Register'}
+				</Button>
+			</form>
+			<p>Don't have an account?</p>
+			<Button inverse onClick={switchModeHandler}>
+				{isLogin ? 'Create Account' : 'Switch to Login'}
+			</Button>
+		</Card>
+	);
 };
 
 export default AuthPage;
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,147 +1,147 @@
 import { CSSProperties, ReactNode, MouseEvent, ReactEventHandler } from 'react';
 
 export type UserInfo = {
-  id: string;
-  name: string;
-  image: string;
-  placeCount: number;
+	id: string;
+	name: string;
+	image: string;
+	placeCount: number;
 };
 
 export type UserInfoList = UserInfo[];
 
 export type AvatarProps = {
-  image: string;
-  alt: string;
-  className?: string;
-  style?: CSSProperties;
-  width?: string;
+	image: string;
+	alt: string;
+	className?: string;
+	style?: CSSProperties;
+	width?: string;
 };
 
 export type UserPlace = {
-  id: string;
-  title: string;
-  description: string;
-  imageUrl: string;
-  address: string;
-  location: {
-    lat: number;
-    lng: number;
-  };
-  creator: string;
+	id: string;
+	title: string;
+	description: string;
+	imageUrl: string;
+	address: string;
+	location: {
+		lat: number;
+		lng: number;
+	};
+	creator: string;
 };
 
 export type UserPlaceArray = UserPlace[];
 
 export type ButtonProps = {
-  size?: 'default' | 'small' | 'big';
-  inverse?: boolean;
-  danger?: boolean;
-  type?: 'button' | 'submit' | 'reset';
-  href?: string;
-  to?: string;
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
-  disabled?: boolean;
-  children: ReactNode;
+	size?: 'default' | 'small' | 'big';
+	inverse?: boolean;
+	danger?: boolean;
+	type?: 'button' | 'submit' | 'reset';
+	href?: string;
+	to?: string;
+	onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+	disabled?: boolean;
+	children: ReactNode;
 };
 
 export type ModalProps = {
-  show: boolean;
-  onCancel: ReactEventHandler;
-  className?: string;
-  style?: CSSProperties;
-  headerClass?: string;
-  header: string;
-  onSubmit?: () => void;
-  contentClass?: string;
-  children: ReactNode;
-  footerClass: string;
-  footer: ReactNode;
+	show: boolean;
+	onCancel: ReactEventHandler;
+	className?: string;
+	style?: CSSProperties;
+	headerClass?: string;
+	header: string;
+	onSubmit?: () => void;
+	contentClass?: string;
+	children: ReactNode;
+	footerClass: string;
+	footer: ReactNode;
 };
 
 export type MapProps = {
-  className?: string;
-  style?: CSSProperties;
-  center: {
-    lat: number;
-    lng: number;
-  };
-  zoom: number;
+	className?: string;
+	style?: CSSProperties;
+	center: {
+		lat: number;
+		lng: number;
+	};
+	zoom: number;
 };
 
 export type InputProps = {
-  element: 'input' | 'textarea';
-  rows?: number;
-  placeholder?: string;
-  type?:
-    | 'text'
-    | 'password'
-    | 'number'
-    | 'email'
-    | 'checkbox'
-    | 'radio'
-    | 'file'
-    | 'date'
-    | 'color'
-    | 'range'
-    | 'hidden';
-  id?: string;
-  className?: string;
-  label: string;
-  errorText?: string;
-  validators: ValidatorType[];
-  onInput: (id: string, value: string, isValid: boolean) => void;
-  value?: string;
-  valid?: boolean;
+	element: 'input' | 'textarea';
+	rows?: number;
+	placeholder?: string;
+	type?:
+		| 'text'
+		| 'password'
+		| 'number'
+		| 'email'
+		| 'checkbox'
+		| 'radio'
+		| 'file'
+		| 'date'
+		| 'color'
+		| 'range'
+		| 'hidden';
+	id?: string;
+	className?: string;
+	label: string;
+	errorText?: string;
+	validators: ValidatorType[];
+	onInput: (id: string, value: string, isValid: boolean) => void;
+	value?: string;
+	valid?: boolean;
 };
 
 export type InputState = {
-  value: string;
-  isValid: boolean;
-  isTouched: boolean;
+	value: string;
+	isValid: boolean;
+	isTouched: boolean;
 };
 
 export type InputAction = {
-  type: 'CHANGE' | 'TOUCH';
-  value: string;
-  validators: ValidatorType[];
+	type: 'CHANGE' | 'TOUCH';
+	value: string;
+	validators: ValidatorType[];
 };
 
 export type ValidatorType = {
-  type: string;
-  val?: number;
+	type: string;
+	val?: number;
 };
 
 export type InputField = {
-  value: string;
-  isValid: boolean;
+	value: string;
+	isValid: boolean;
 };
 
 export type FormState =
-  | {
-      title: InputField;
-      description: InputField;
-      address?: InputField;
-    }
-  | {
-      name?: InputField;
-      email: InputField;
-      password: InputField;
-    };
+	| {
+			title: InputField;
+			description: InputField;
+			address?: InputField;
+	}
+	| {
+			name?: InputField;
+			email: InputField;
+			password: InputField;
+	};
 
 export type FormReducerState = {
-  inputs: FormState;
-  isValid: boolean;
+	inputs: FormState;
+	isValid: boolean;
 };
 
 export type FormAction =
-  | {
-      type: 'INPUT_CHANGE';
-      value: string;
-      isValid: boolean;
-      inputId: string;
-    }
-  | {
-      type: 'SET_DATA';
-      inputs: FormState;
-      formIsValid: boolean;
-    };
+	| {
+			type: 'INPUT_CHANGE';
+			value: string;
+			isValid: boolean;
+			inputId: string;
+	}
+	| {
+			type: 'SET_DATA';
+			inputs: FormState;
+			formIsValid: boolean;
+	};
