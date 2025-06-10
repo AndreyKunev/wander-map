34b40c351b10a7be211755a2a4f586f4829405c8
Andrey Kunev <akunev45@gmail.com>
2025-06-10 23:25:37 +0300

    add#24: replaced getUsers with custom http hook

--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -4,41 +4,32 @@ import UsersList from '../../components/UsersList/UsersList';
 import ErrorModal from '../../components/ErrorModal/ErrorModal';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 
+import { useHttpClient } from '../../hooks/http-hook';
+
 const Users = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState<string | null>(null);
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
 	const [loadedUsers, setLoadedUsers] = useState([]);
 
 	useEffect(() => {
-		const sendReq = async () => {
-			setIsLoading(true);
+		const getUsers = async () => {
 			try {
-				const res = await fetch('http://localhost:3001/api/users/');
+				const data = await sendRequest(
+					'http://localhost:3001/api/users/'
+				);
 
-				const resData = await res.json();
-
-				if (!res.ok) {
-					throw new Error(resData.message);
-				}
-
-				setLoadedUsers(resData.users);
+				setLoadedUsers(data.users);
 			} catch (err) {
 				if (err instanceof Error) {
-					setError(err.message);
+					console.log(err.message);
 				}
 			}
-			setIsLoading(false);
 		};
-		sendReq();
-	}, []);
-
-	const errorHandler = () => {
-		setError(null);
-	};
+		getUsers();
+	}, [sendRequest]);
 
 	return (
 		<>
-			<ErrorModal error={error} onClear={errorHandler} />
+			<ErrorModal error={error} onClear={clearError} />
 			{isLoading && (
 				<div className='center'>
 					<LoadingSpinner asOverlay />
