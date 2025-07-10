6ec0b8e373c16421e4427a963d6ae5896a5334e8
Andrey Kunev <akunev45@gmail.com>
2025-07-10 23:39:44 +0300

    add#24: added delete request to placeItem

--- frontend/src/components/PlaceList/PlaceItem/PlaceItem.tsx
+++ frontend/src/components/PlaceList/PlaceItem/PlaceItem.tsx
@@ -4,85 +4,98 @@ import Card from '../../Card/Card';
 import Button from '../../FormElements/Button/Button';
 import Modal from '../../Modal/Modal';
 import Map from '../../Map/Map';
-import { AuthContext } from '../../../context/auth-context';
 
-import './PlaceItem.css';
+import { AuthContext } from '../../../context/auth-context';
+import { useHttpClient } from '../../../hooks/http-hook';
 
 import { UserPlace } from '../../../types/types';
 
+import './PlaceItem.css';
+
 const PlaceItem: FC<{ place: UserPlace }> = ({ place }) => {
-  const auth = useContext(AuthContext);
-  const [showMap, setShowMap] = useState(false);
-  const [showConfirmModal, setShowConfirmModal] = useState(false);
+	const auth = useContext(AuthContext);
+	const [showMap, setShowMap] = useState(false);
+	const [showConfirmModal, setShowConfirmModal] = useState(false);
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  const toggleMapHandler = () => setShowMap(!showMap);
+	const toggleMapHandler = () => setShowMap(!showMap);
 
-  const toggleConfirmModal = () => setShowConfirmModal(!showConfirmModal);
+	const toggleConfirmModal = () => setShowConfirmModal(!showConfirmModal);
 
-  const confirmDeleteHandler = () => {
-    setShowConfirmModal(false);
-    console.log('Deleting...');
-  };
+	const confirmDeleteHandler = async () => {
+		setShowConfirmModal(false);
 
-  return (
-    <>
-      <Modal
-        show={showMap}
-        onCancel={toggleMapHandler}
-        header={place.address}
-        contentClass="place-item__modal-content"
-        footerClass="place-item__modal-actions"
-        footer={<Button onClick={toggleMapHandler}>Close</Button>}
-      >
-        <div className="map-container">
-          <Map center={place.location} zoom={16} />
-        </div>
-      </Modal>
-      <Modal
-        show={showConfirmModal}
-        onCancel={toggleConfirmModal}
-        header="Are you sure?"
-        footerClass="place-item__modal-actions"
-        footer={
-          <>
-            <Button inverse onClick={toggleConfirmModal}>
-              Cancel
-            </Button>
-            <Button danger onClick={confirmDeleteHandler}>
-              Delete
-            </Button>
-          </>
-        }
-      >
-        <p>Deleting a wander point is irreversible.</p>
-      </Modal>
-      <li className="place-item">
-        <Card className="place-item__content">
-          <div className="place-item__image">
-            <img src={place.image} alt={place.title} />
-          </div>
-          <div className="place-item__info">
-            <h2>{place.title}</h2>
-            <h3>{place.address}</h3>
-            <p>{place.description}</p>
-          </div>
-          <div className="place-item__actions">
-            <Button inverse onClick={toggleMapHandler}>
-              View on Map
-            </Button>
-            {auth.isLoggedIn && (
-              <Button to={`/places/${place.id}`}>Edit</Button>
-            )}
-            {auth.isLoggedIn && (
-              <Button danger onClick={toggleConfirmModal}>
-                Delete
-              </Button>
-            )}
-          </div>
-        </Card>
-      </li>
-    </>
-  );
+		try {
+			await sendRequest(
+				`http://localhost:3001/api/places/${place.id}`,
+				'DELETE'
+			);
+		} catch (err) {
+      // TODO - Replace console.log and console.errors with something better in catch block where sendRequest is used
+      console.log(err);
+      
+    }
+	};
+
+	return (
+		<>
+			<Modal
+				show={showMap}
+				onCancel={toggleMapHandler}
+				header={place.address}
+				contentClass='place-item__modal-content'
+				footerClass='place-item__modal-actions'
+				footer={<Button onClick={toggleMapHandler}>Close</Button>}
+			>
+				<div className='map-container'>
+					<Map center={place.location} zoom={16} />
+				</div>
+			</Modal>
+			<Modal
+				show={showConfirmModal}
+				onCancel={toggleConfirmModal}
+				header='Are you sure?'
+				footerClass='place-item__modal-actions'
+				footer={
+					<>
+						<Button inverse onClick={toggleConfirmModal}>
+							Cancel
+						</Button>
+						<Button danger onClick={confirmDeleteHandler}>
+							Delete
+						</Button>
+					</>
+				}
+			>
+				<p>Deleting a wander point is irreversible.</p>
+			</Modal>
+			<li className='place-item'>
+				<Card className='place-item__content'>
+					<div className='place-item__image'>
+						<img src={place.image} alt={place.title} />
+					</div>
+					<div className='place-item__info'>
+						<h2>{place.title}</h2>
+						<h3>{place.address}</h3>
+						<p>{place.description}</p>
+					</div>
+					<div className='place-item__actions'>
+						<Button inverse onClick={toggleMapHandler}>
+							View on Map
+						</Button>
+						{auth.isLoggedIn && (
+							<Button to={`/places/${place.id}`}>Edit</Button>
+						)}
+						{auth.isLoggedIn && (
+							<Button danger onClick={toggleConfirmModal}>
+								Delete
+							</Button>
+						)}
+					</div>
+				</Card>
+			</li>
+		</>
+	);
 };
 
 export default PlaceItem;
